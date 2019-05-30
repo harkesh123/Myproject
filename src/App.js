d4c459c22c6fea9673ab46ad9fa4dd7e620ec19d
Harkesh <harkesh.d@gmail.com>
2019-05-30 23:43:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="border" style={{ padding: "25px" }}>
+        <h3 style={{ textAlign: "left" }}> {this.props.title}</h3>
+        <div class="dropdown-divider" />
+        <p>{this.props.body}</p>
+        <button type="button" class="btn btn-primary">
+          Edit
+        </button>
+        <button type="button" class="btn btn-warning">
+          Delete
+        </button>
+
+        <div class="dropdown-divider" />
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  state = {
+    result: []
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ result: json });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div
+          style={{ textAlign: "center" }}
+          className="p-3 mb-2 bg-primary text-white"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h2>List</h2>
+        </div>
+        {this.state.result.map(list => {
+          return <Card key={list.id} title={list.title} body={list.body} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
